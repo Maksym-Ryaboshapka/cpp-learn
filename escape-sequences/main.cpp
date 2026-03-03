de9311f7148eaf3e2d4af5a327a9520551a32f0b
Maksym-Ryaboshapka <testbs777888@gmail.com>
2026-03-03 11:09:04 +0200

    learn escape sequences

---
+++ escape-sequences/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hello!\n"; // \n - перенос на новую строку
+
+  std::cout << "\tHello!\n"; // \t - табуляция
+
+  std::cout << "Hello!\b\n"; // \b - удаление последнего выводимого символа
+
+  std::cout << "\"Hello!\"\n"; // \" - экранирование кавычки
+
+  return 0;
+}
