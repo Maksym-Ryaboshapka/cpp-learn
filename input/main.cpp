e8c8d60a811895b709db4dded21d04122c5db4fa
Maksym-Ryaboshapka <testbs777888@gmail.com>
2026-03-04 19:29:16 +0200

    learn input

---
+++ input/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+  // int var1, var2;
+  //
+  // std::cout << "Введите первое число: ";
+  // std::cin >> var1;
+  //
+  // std::cout << "Введите второе число: ";
+  // std::cin >> var2;
+  //
+  // std::cout << "Число 1 = " << var1 << std::endl << "Число 2 = " << var2 << std::endl;
+
+  int var1, var2;
+
+  std::cout << "Введите 2 числа: ";
+  std::cin >> var1 >> var2;
+
+  std::cout << "Число 1 = " << var1 << std::endl << "Число 2 = " << var2 << std::endl;
+
+  return 0;
+}
